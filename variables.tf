9c7229949d5f46a5a50bc6500b975925286b1488
Anders Soder <anders.soder@kindredgroup.com>
2019-04-01 16:34:13 +0200

    removed unused vars

--- variables.tf
+++ variables.tf
@@ -373,12 +373,6 @@ variable "bootstrap_extra_args" {
   description = "Passed to the bootstrap.sh script to enable --kublet-extra-args or --use-max-pods."
 }
 
-variable "cfn_stack_description" {
-  type        = "string"
-  default     = "Managed by Terraform, do not manually modify. Creates Autoscaling group with rolling update support."
-  description = "Sets the description on the Cloudformation stack."
-}
-
 variable "cfn_creation_policy_timeout" {
   type        = "string"
   default     = "PT10M"
@@ -390,10 +384,10 @@ variable "cfn_creation_policy_min_successful_instances_percent" {
   description = "Specifies the percentage of instances in an Auto Scaling replacement update that must signal success for the update to succeed. You can specify a value from 0 to 100. AWS CloudFormation rounds to the nearest tenth of a percent. For example, if you update five instances with a minimum successful percentage of 50, three instances must signal success. If an instance doesn't send a signal within the time specified by the Timeout property, AWS CloudFormation assumes that the instance wasn't created."
 }
 
-variable "cfn_update_policy_min_successful_instances_percent" {
-  default     = 80
-  description = "description"
-}
+# variable "cfn_update_policy_min_successful_instances_percent" {
+#   default     = 80
+#   description = "description"
+# }
 
 variable "cfn_update_policy_max_batch_size" {
   default     = 1
